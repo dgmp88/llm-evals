e9fd31a42296b6be561f3122b5cd011a4834803b
George Prichard <dgmprichard@gmail.com>
2025-02-02 22:35:47 +0000

    Update plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,34 @@
+import altair as alt
 import pandas as pd
 import streamlit as st
 
 from llm_evals.db import EvalResult
 
-results = [r for r in EvalResult.select().dicts()]
+# Pull in the data
+results = pd.DataFrame([r for r in EvalResult.select().dicts()])
 
-results = pd.DataFrame(results)
-st.markdown(results.to_markdown())
+results = (
+    results.groupby(["model_name", "eval_name"])
+    .result.mean()
+    .sort_values(ascending=False)
+    .reset_index()
+    .assign(
+        model_name=lambda x: x.model_name.str.replace("together_ai/", ""),
+    )
+)
+
+
+st.altair_chart(
+    alt.Chart(results)
+    .mark_bar()
+    .encode(
+        x=alt.X(
+            "model_name",
+            sort=None,
+            title="Model",
+            # axis=alt.Axis(labelAngle=-45)
+        ),
+        y=alt.Y("result", title="Score"),
+    ),
+    use_container_width=True,
+)
