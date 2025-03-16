34346c8fd5b0466c9e5d2693de264fc3ce16e61c
George Prichard <dgmprichard@gmail.com>
2025-03-16 14:33:54 +0000

    WIP

--- fhtml.py
+++ fhtml.py
@@ -1,3 +1,4 @@
+import numpy as np
 import plotly.express as px
 from fasthtml.common import Div, NotStr, Script, Titled, fast_app, serve
 
@@ -16,13 +17,32 @@ def get():
 
     plots = []
     for eval_name, df in results.groupby("eval_name"):
-        fig = px.bar(df, x="result", y="model_name")
+        colour = None
+
+        if "tictactoe" in eval_name:
+            print("eval_name", eval_name)
+            # red if <0, green if >0
+            df["colour"] = np.where(df["result"] < 0, "red", "green")
+            colour = "colour"
+        fig = px.bar(
+            df,
+            x="result",
+            y="model_name",
+            color=colour,
+            color_discrete_map={"red": "red", "green": "green"},
+        )
 
         fig.update_layout(
             title=eval_name,
             xaxis_title="Score",
-            yaxis_title="Model",
+            yaxis_title=None,
+            # remove legend
+            showlegend=False,
         )
+
+        if "tictactoe" in eval_name:
+            # Change xlims
+            fig.update_xaxes(range=[-1, 1])
 
         plots.append(NotStr(fig.to_html(full_html=False, include_plotlyjs=False)))
 
