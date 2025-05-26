0470ba7705fde1bfa4a184a540cd3c78e095c92e
George Prichard <dgmprichard@gmail.com>
2025-05-26 11:43:11 +0100

    Move to UV

--- evals/util/llm.py
+++ evals/util/llm.py
@@ -1,7 +1,10 @@
+import dotenv
 from litellm import batch_completion as lite_batch_completion
 from litellm import completion as lite_completion  # type: ignore
 
 from evals.types import Message, Model
+
+dotenv.load_dotenv()
 
 DEFAULT_TEMPERATURE = 0.001  # 0 breaks some providers, so just set it super low
 DEFAULT_MAX_TOKENS = 10
@@ -26,10 +29,21 @@ def batch_completion(model: Model, messages: list[list[Message]]) -> list[str]:
         max_tokens=DEFAULT_MAX_TOKENS,
         temperature=DEFAULT_TEMPERATURE,
         num_retries=2,
+        n=1,
     )
     return [str(response.choices[0].message.content) for response in responses]
 
 
-if __name__ == "__main__":
-    result = completion("gpt-4o-2024-08-06", [Message(content="hello", role="user")])
+def test_llm(model: str):
+    result = completion(model, [Message(content="hello", role="user")])
     print(result)
+
+
+if __name__ == "__main__":
+    import fire
+
+    fire.Fire(
+        {
+            "test_llm": test_llm,
+        }
+    )
